b1d2721dc5474cee7533867bd8cc6a1539fab99c
igor <sas.ihor.new@gmail.com>
2019-06-15 13:57:30 +0300

    added REST and SOAP clients with one test class

--- src/main/java/com/epam/web/rest/UserRestServiceClient.java
+++ src/main/java/com/epam/web/rest/UserRestServiceClient.java
@@ -16,32 +16,27 @@ import org.codehaus.jackson.type.TypeReference;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 public class UserRestServiceClient implements UserService {
-
-
-    public Logger LOGGER = LogManager.getLogger(UserRestServiceClient.class);
-
-    public static final String BASE_ADDRESS = "http://localhost:8080/IgorSasService/rest";
-
-    private ClientConfig clientConfig;
+    private Logger LOGGER = LogManager.getLogger(UserRestServiceClient.class);
+    private static final String BASE_ADDRESS = "http://localhost:8080/IgorSasService/rest";
     private Client client;
 
     public UserRestServiceClient() {
-        clientConfig = new DefaultClientConfig();
+        ClientConfig clientConfig = new DefaultClientConfig();
         clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
         clientConfig = new DefaultClientConfig();
         client = Client.create(clientConfig);
     }
 
     public List<User> getAllUsers() {
-        LOGGER.info("getAllBooks method");
-        ObjectMapper mapper = new ObjectMapper();
+        LOGGER.info("get all users rest");
 
-        String uri = BASE_ADDRESS + "/users";
-        LOGGER.info("path:" + uri);
+        String url = BASE_ADDRESS + "/users";
+        LOGGER.info("path:" + url);
 
-        WebResource webResource = client.resource(uri);
+        WebResource webResource = client.resource(url);
 
         ClientResponse response = webResource.accept("application/json;encoding=UTF-8")
                 .get(ClientResponse.class);
@@ -50,9 +45,10 @@ public class UserRestServiceClient implements UserService {
         List<User> list = null;
 
         try {
-            list = mapper.readValue(usersJson, new TypeReference<List<User>>() {
+            list = new ObjectMapper().readValue(usersJson, new TypeReference<List<User>>() {
             });
         } catch (IOException e) {
+            LOGGER.error(e.getMessage());
             e.printStackTrace();
         }
 
@@ -60,20 +56,21 @@ public class UserRestServiceClient implements UserService {
     }
 
     public List<Role> getRoles() throws ServiceException {
-        LOGGER.info("get Roles for user");
+        LOGGER.info("get Roles for user rest");
 
         ObjectMapper mapper = new ObjectMapper();
-        String uri = BASE_ADDRESS + "/role";
-        LOGGER.info("path:" + uri);
+        String url = BASE_ADDRESS + "/role";
+        LOGGER.info("path:" + url);
 
-        WebResource webResource = client.resource(uri);
+        WebResource webResource = client.resource(url);
 
         ClientResponse response = webResource.accept("application/json;encoding=UTF-8").type("application/json")
                 .get(ClientResponse.class);
 
         List<Role> list = null;
         if (response.getStatus() != 200) {
-            throw buildException(response, mapper);
+            LOGGER.error("Can't get roles for current user. Are you logged in?");
+            throw getException(response, mapper);
         } else {
             String jsonRoles = response.getEntity(String.class);
 
@@ -81,6 +78,7 @@ public class UserRestServiceClient implements UserService {
                 list = mapper.readValue(jsonRoles, new TypeReference<List<Role>>() {
                 });
             } catch (IOException e) {
+                LOGGER.error(e.getMessage());
                 e.printStackTrace();
             }
 
@@ -89,27 +87,28 @@ public class UserRestServiceClient implements UserService {
     }
 
     public List<User> getUsersByRole(String role) throws ServiceException {
-        LOGGER.info("get Users by Role");
+        LOGGER.info("get Users by Role rest");
 
         ObjectMapper mapper = new ObjectMapper();
-        String uri = BASE_ADDRESS + "/role/"+role;
-        LOGGER.info("path:" + uri);
+        String url = BASE_ADDRESS + "/role/" + role;
+        LOGGER.info("path:" + url);
 
-        WebResource webResource = client.resource(uri);
+        WebResource webResource = client.resource(url);
 
         ClientResponse response = webResource.accept("application/json;encoding=UTF-8").type("application/json")
                 .get(ClientResponse.class);
 
         List<User> list = null;
         if (response.getStatus() != 200) {
-            throw buildException(response, mapper);
+            LOGGER.error("Can't get users by role. Please, check that role is correct");
+            throw getException(response, mapper);
         } else {
             String jsonRoles = response.getEntity(String.class);
-
             try {
                 list = mapper.readValue(jsonRoles, new TypeReference<List<User>>() {
                 });
             } catch (IOException e) {
+                LOGGER.error(e.getMessage());
                 e.printStackTrace();
             }
 
@@ -117,38 +116,36 @@ public class UserRestServiceClient implements UserService {
         return list;
     }
 
-    public boolean addUser(User user) throws ServiceException {
-        LOGGER.info("add new user");
+    public boolean addUser(User user) {
+        LOGGER.info("add new user by rest");
 
         ObjectMapper mapper = new ObjectMapper();
-        String uri = BASE_ADDRESS + "/user/add";
-        LOGGER.info("path:" + uri);
+        String url = BASE_ADDRESS + "/user/add";
+        LOGGER.info("path:" + url);
 
-        WebResource webResource = client.resource(uri);
+        WebResource webResource = client.resource(url);
 
         ClientResponse response = null;
         try {
             response = webResource.accept("application/json;encoding=UTF-8").type("application/json")
                     .post(ClientResponse.class, mapper.writeValueAsString(user));
         } catch (IOException e) {
+            LOGGER.error(e.getMessage());
             e.printStackTrace();
         }
 
-        if (response.getStatus() != 200) {
-            throw buildException(response, mapper);
-        }
-        return true;
+        return (!Objects.isNull(response) ? response.getStatus() : 0) == 200;
     }
 
-    public boolean removeUser(User user) throws ServiceException {
-        LOGGER.info("removeUser");
+    public boolean removeUser(User user) {
+        LOGGER.info("remove User by rest");
 
         ObjectMapper mapper = new ObjectMapper();
 
-        String uri = BASE_ADDRESS + "/user/delete";
-        LOGGER.info("path:" + uri);
+        String url = BASE_ADDRESS + "/user/delete";
+        LOGGER.info("path:" + url);
 
-        WebResource webResource = client.resource(uri);
+        WebResource webResource = client.resource(url);
 
         ClientResponse response = null;
         try {
@@ -158,14 +155,11 @@ public class UserRestServiceClient implements UserService {
             e.printStackTrace();
         }
 
-        if (response.getStatus() != 200) {
-            throw buildException(response, mapper);
-        }
-        return true;
+        return (!Objects.isNull(response) ? response.getStatus() : 0) == 200;
     }
 
-    public boolean logIn(LoginModel loginModel) throws ServiceException {
-        LOGGER.info("loggin in user");
+    public boolean logIn(LoginModel loginModel) {
+        LOGGER.info("logging in user by rest");
 
         ObjectMapper mapper = new ObjectMapper();
         String uri = BASE_ADDRESS + "/login";
@@ -181,13 +175,10 @@ public class UserRestServiceClient implements UserService {
             e.printStackTrace();
         }
 
-        if (response.getStatus() != 200) {
-            throw buildException(response, mapper);
-        }
-        return true;
+        return (!Objects.isNull(response) ? response.getStatus() : 0) == 200;
     }
 
-    private ServiceException buildException(ClientResponse response, ObjectMapper mapper) throws ServiceException {
+    private ServiceException getException(ClientResponse response, ObjectMapper mapper) {
         String jsonFaultInfo = response.getEntity(String.class);
 
         UserFault faultInfo = null;
@@ -196,8 +187,6 @@ public class UserRestServiceClient implements UserService {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return new ServiceException(faultInfo.getMessage());
-
+        return new ServiceException("Error", faultInfo);
     }
-
 }
---
+++ src/test/java/com/epam/web/GetUsersTest.java
@@ -0,0 +1,47 @@
+package com.epam.web;
+
+import com.epam.web.soap.ServiceException;
+import com.epam.web.soap.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class GetUsersTest {
+    private Logger LOGGER = LogManager.getLogger(GetUsersTest.class);
+
+    @DataProvider
+    private Object[] typeService(){
+        return new Object[]{ServiceFactory.REST, ServiceFactory.SOAP};
+    }
+
+    @Test(dataProvider = "typeService")
+    public void getAllUsersTest(String typeService){
+        LOGGER.info("get all users by " + typeService);
+        UserService service = ServiceFactory.getUserService(typeService);
+        List<User> users = service.getAllUsers();
+        LOGGER.info("users: " + users);
+        Assert.assertFalse(users.isEmpty(), "user list isn't empty");
+    }
+
+    @Test(dataProvider = "typeService", expectedExceptions = ServiceException.class)
+    public void getUsersByIncorrectRoleTest(String typeService) throws ServiceException {
+        LOGGER.info("get users by incorrect role " + typeService);
+        String role = "humunkul";
+        UserService service = ServiceFactory.getUserService(typeService);
+        List<User> users = service.getUsersByRole(role);
+    }
+
+    @Test(dataProvider = "typeService")
+    public void getUsersByRole(String typeService) throws ServiceException {
+        LOGGER.info("get users by incorrect role " + typeService);
+        String role = "user";
+        UserService service = ServiceFactory.getUserService(typeService);
+        List<User> users = service.getUsersByRole(role);
+        LOGGER.info("users: " + users);
+        Assert.assertFalse(users.isEmpty(), "user list isn't empty");
+    }
+}
